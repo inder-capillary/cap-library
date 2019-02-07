c43489438b8d22d5d6257e484d4cd0b5869fa380
sakshi-kataria-capillary <sakshi.kataria@capillarytech.com>
2019-02-07 11:35:11 +0530

    test cases for checkbox and switch

--- components/CapCheckbox/tests/index.test.js
+++ components/CapCheckbox/tests/index.test.js
@@ -1,10 +1,30 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import React from 'react'; // eslint-disable-line no-unused-vars
+import {mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CapCheckbox from '../index';
 
-// import CapCheckbox from '../index';
+configure({ adapter: new Adapter() });
 
 describe('<CapCheckbox />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('CapCheckbox renders correctly', () => {
+    const tree = renderer.create(<CapCheckbox />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('check the props is defined', () => {
+    const props = {
+      defaultChecked: true,
+    };
+    const CapCheckboxComponent = mount(<CapCheckbox {...props} />);
+    expect(CapCheckboxComponent.prop('defaultChecked')).toBeDefined();
+  });
+
+  it('render size input correctly with small value', () => {
+    const props = {
+      defaultChecked: true,
+    };
+    const CapCheckboxComponent = mount(<CapCheckbox {...props} />);
+    expect(CapCheckboxComponent.prop('defaultChecked')).toEqual(true);
   });
 });
--- components/CapSlider/tests/index.test.js
+++ components/CapSlider/tests/index.test.js
@@ -1,10 +1,30 @@
 import renderer from 'react-test-renderer';
 import React from 'react'; // eslint-disable-line no-unused-vars
+import {mount, configure} from 'enzyme';
+
+import Adapter from 'enzyme-adapter-react-16';
 import CapSlider from '../index';
 
+configure({ adapter: new Adapter() });
 describe('<CapSlider />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<CapSlider />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('render defaultValue input correctly with 40 value', () => {
+    const props = {
+      defaultValue: 40,
+    };
+    const sliderInputComponent = mount(<CapSlider {...props} />);
+    expect(sliderInputComponent.prop('defaultValue')).toEqual(40);
+  });
+
+  it('check the value is defined', () => {
+    const props = {
+      defaultValue: 40,
+    };
+    const sliderInputComponent = mount(<CapSlider {...props} />);
+    expect(sliderInputComponent.prop('defaultValue')).toBeDefined();
   });
 });
