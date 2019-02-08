b8904e31d615d172c95971549133ae8d0f3dafa5
Swathi8512 <swathi.venkatesh@capillarytech.com>
2019-02-08 09:00:15 +0530

    Remove property table from doc as there is no new properties as of now

--- src/CapTableDoc/index.js
+++ src/CapTableDoc/index.js
@@ -2,19 +2,19 @@
 * CapTableDoc
 */
 import React, { Component } from "react";
-import PropertyTable from '../../helpers/PropertyTable';
+// import PropertyTable from '../../helpers/PropertyTable';
 import CapTable from "../../components/CapTable";
 import "./info.scss";
 
-const infoData = [
-  {
-    key: 1,
-    property: "-",
-    description: "-",
-    type: "-",
-    default: "-",
-  },
-];
+// const infoData = [
+//   {
+//     key: 1,
+//     property: "-",
+//     description: "-",
+//     type: "-",
+//     default: "-",
+//   },
+// ];
 
 const dataSource = [{
   key: '1',
@@ -49,7 +49,7 @@ export default class CapTableDoc extends Component { // eslint-disable-line reac
         <div className="cap-table-showcase">
           <CapTable columns={columns} dataSource={dataSource} />
         </div>
-        <PropertyTable data={infoData} />
+        {/* <PropertyTable data={infoData} /> */}
         <div style={{marginTop: '24px'}}>
           <b>NOTE: </b>
           This component is the extended version of ant design
