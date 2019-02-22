9463a8c6a7984e0b28ab082795fd6fb1075582ae
Sakshi-kataria-Capillary <42139409+Sakshi-kataria-Capillary@users.noreply.github.com>
2019-02-22 16:47:09 +0530

    Cap card sms (#57)
    * cap card sms
    
    * cap card sms

--- components/CapCard/index.js
+++ components/CapCard/index.js
@@ -8,23 +8,45 @@ import React from 'react';
 import './_capCard.scss';
 import PropTypes from 'prop-types';
 import { Card} from "antd";
+import CapPopover from '../CapPopover';
+import CapButton from '../CapButton';
+
+import { View as ViewIcon, SvgMore as MoreIcon} from "../assets/icons";
 // import styled from 'styled-components';
 const classNames = require('classnames');
-
+const {Meta} = Card;
 class CapCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const { children, className, ...rest} = this.props;
+    const { className, cardList, type} = this.props;
     return (
-      <Card {...rest} className={classNames("cap-card", className)}>
-        {React.Children.toArray(children)}
-      </Card>
+      <div className={classNames(`cap-card-v2 cap-card-${type}`, className)}>
+        {cardList ? (
+          cardList.map((c, i) => {
+            const {content, viewIcon, moreIcon, onClick, hover, ...rest} = c;
+            const icon = [];
+            if (viewIcon) {
+              icon.push(<ViewIcon />);
+            }
+            if (moreIcon) {
+              icon.push(<MoreIcon />);
+            }
+            return (
+              <CapPopover className={hover ? '' : 'disable-hover'} content={hover ? <CapButton onClick={onClick} key={i} visible={false}>Select</CapButton> : false}>
+                <Card {...rest} key={i} extra={icon}>
+                  <Meta key={i} description={content} />
+                </Card>
+              </CapPopover>
+            );
+          }) ) : <div></div>}
+      </div>
     );
   }
 }
 
 CapCard.propTypes = {
-  children: PropTypes.any,
   className: PropTypes.string,
+  cardList: PropTypes.array,
+  type: PropTypes.string,
 };
 
 export default CapCard;
