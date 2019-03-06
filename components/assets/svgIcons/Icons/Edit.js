911b73ecdf0c676f374a5b390cd0242391687ab4
Jagrati Tomar <jagrati.tomar@capillarytech.com>
2019-03-06 12:17:12 +0530

    CapIcons

--- components/assets/svgIcons/Icons/Edit.js
+++ components/assets/svgIcons/Icons/Edit.js
@@ -8,21 +8,10 @@ const SvgEdit = (props) => (
     fill="currentColor"
     {...props}
   >
-    <defs>
-      <path
-        d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06zM17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83a.996.996 0 0 0 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"
-        id="edit_svg__a"
-      />
-    </defs>
-    <g fill="none" fillRule="evenodd">
-      <mask id="edit_svg__b" fill="#fff">
-        <use xlinkHref="#edit_svg__a" />
-      </mask>
-      <use fill="#7A869A" xlinkHref="#edit_svg__a" />
-      <g mask="url(#edit_svg__b)" fill="#091E42">
-        <path d="M0 0h24v24H0z" />
-      </g>
-    </g>
+    <path
+      d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06zM17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83a.996.996 0 0 0 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"
+      id="edit_svg__a"
+    />
   </svg>
 );
 
