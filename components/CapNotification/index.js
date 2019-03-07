7baff1a5b8dc28fdb7b425fc369f73eccdd30036
Jagrati Tomar <jagrati.tomar@capillarytech.com>
2019-03-07 13:17:15 +0530

    notification

--- components/CapNotification/index.js
+++ components/CapNotification/index.js
@@ -6,31 +6,43 @@
 import React from 'react';
 import { notification } from 'antd';
 import classNames from 'classnames';
-import { CapHeading } from '../index';
+import { CapHeading, CapIcon } from '../index';
 
-const { info, warning, warn, success, close, destroy } = notification;
+import './_capNotification.scss';
+
+const { info, warning, warn, close, destroy } = notification;
 
 const clsPrefix = 'cap-notification-v2';
 
 const openNotification = (props, type) => {
   const { className, message, description, ...rest } = props;
-  notification.open({
+  const notificationProps = {
     message: <CapHeading type="h4">{message}</CapHeading>,
     description: <CapHeading type="h6">{description}</CapHeading>,
     ...rest,
     className: classNames(clsPrefix, className, type),
-  });
+  };
+  if (type === 'error') {
+    notificationProps.icon = <CapIcon className="error" type="alert" />;
+  } else if (type === 'success') {
+    notificationProps.icon = <CapIcon className="success" type="check-filled" />;
+  }
+  notification.open(notificationProps);
 };
 
 const errorNotification = (props) => {
   openNotification(props, 'error');
 };
 
+const successNotification = (props) => {
+  openNotification(props, 'success');
+};
+
 
 const CapNotification = {};
 
 CapNotification.open = openNotification;
-CapNotification.success = success;
+CapNotification.success = successNotification;
 CapNotification.error = errorNotification;
 CapNotification.info = info;
 CapNotification.warning = warning;
--- src/CapNotificationDoc/index.js
+++ src/CapNotificationDoc/index.js
@@ -4,7 +4,7 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 //import PropertyTable from '../../helpers/PropertyTable';
-import { CapNotification, CapButton, CapHeading } from "../../components";
+import { CapNotification, CapButton, CapHeading, CapIcon } from "../../components";
 import "./info.scss";
 
 // const infoData = [
@@ -21,24 +21,55 @@ const Heading = styled(CapHeading)`
   padding-bottom: 8px;
 `;
 
-const openNotification = (type) => {
-  if (type === 'error') {
-    CapNotification.error({
-      message: 'Notification Title',
-      description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-      onClick: () => {
-      },
-      duration: 0,
-    });
-  } else {
-    CapNotification.open({
-      message: 'Notification Title',
-      description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-      onClick: () => {
-      },
-      duration: 0,
-    });
-  }
+const openErrorNotification = () => {
+  CapNotification.error({
+    message: 'Notification Title',
+    description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    onClick: () => {
+    },
+    duration: 0,
+  });
+};
+
+const openSuccessNotification = () => {
+  CapNotification.success({
+    message: 'Notification Title',
+    description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    onClick: () => {
+    },
+    duration: 0,
+  });
+};
+
+const openDefaultNotification = () => {
+  CapNotification.open({
+    message: 'Notification Title',
+    description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    onClick: () => {
+    },
+    duration: 0,
+    key: 'default_notification',
+  });
+};
+
+const closeDefaultNotification = () => {
+  CapNotification.close('default_notification');
+};
+
+const openDefaultNotificationWithIcon = () => {
+  CapNotification.open({
+    message: 'Notification Title',
+    description: (
+      <div>
+        <div>This is the content of the notification. This is the content of the notification</div>
+        <CapButton style={{ marginTop: "24px" }}>Continue</CapButton>
+      </div>
+    ),
+    onClick: () => {
+    },
+    duration: 0,
+    icon: <CapIcon type="gallery" />,
+  });
 };
 
 export default class CapNotificationDoc extends Component { // eslint-disable-line react/prefer-stateless-function
@@ -46,10 +77,25 @@ export default class CapNotificationDoc extends Component { // eslint-disable-li
     return (
       <div className="cap-notification-info">
         <div className="cap-notification-showcase">
-          <CapButton type="primary" onClick={() => openNotification()}>default notification box</CapButton>
+          <div style={{ marginBottom: '24px' }}>
+            <b>NOTE: </b>
+            This component is the extended version of ant design
+            <a href="https://ant.design/components/notification/#header"> Notification </a>
+            component. Please refer their component for detailed explaination of component and supported props.
+          </div>
+          <CapButton type="primary" onClick={openDefaultNotification}>default notification box</CapButton>
           <br />
           <br />
-          <CapButton type="primary" onClick={() => openNotification('error')}>Error Notification box</CapButton>
+          <CapButton type="primary" onClick={closeDefaultNotification}>Close default notification box</CapButton>
+          <br />
+          <br />
+          <CapButton type="primary" onClick={openErrorNotification}>Error Notification box</CapButton>
+          <br />
+          <br />
+          <CapButton type="primary" onClick={openSuccessNotification}>success Notification box</CapButton>
+          <br />
+          <br />
+          <CapButton type="primary" onClick={openDefaultNotificationWithIcon}>Message with Icon</CapButton>
         </div>
         {/* <PropertyTable data={infoData} /> */}
         <div style={{ marginTop: '24px' }}>
@@ -64,12 +110,6 @@ export default class CapNotificationDoc extends Component { // eslint-disable-li
           <Heading>CapNotification.open(config)</Heading>
           <Heading>CapNotification.close(key: string)</Heading>
           <Heading>CapNotification.destroy()</Heading>
-        </div>
-        <div style={{ marginTop: '24px' }}>
-          <b>NOTE: </b>
-          This component is the extended version of ant design
-          <a href="https://ant.design/components/notification/#header"> Notification </a>
-          component. Please refer their component for detailed explaination of component and supported props.
         </div>
       </div>
     );
