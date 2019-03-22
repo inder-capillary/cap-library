0b117701f850a0aa22d9948016479681d9d1e6f7
Jagrati Tomar <37534926+jagratiTomar@users.noreply.github.com>
2019-03-22 17:46:07 +0530

    Cap table pagination fix (#107)
    * capTable pagination fix
    
    * Call set pagination if content not overflowed
    
    * undo unnecessary changes
    
    * undo unnecessary changes
    
    * add condition
    
    * fix doc

--- components/CapTable/CapTable.js
+++ components/CapTable/CapTable.js
@@ -3,28 +3,42 @@
 */
 import React from "react";
 import PropTypes from 'prop-types';
+import { throttle } from 'lodash';
 import { Table } from "antd";
 import "./_capTable.scss";
 const classNames = require('classnames');
 
 export default class CapTable extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.onScrollThrottle = throttle(this.onScrollListTable, 50);
+    this.setPaginationCalled = false;
+  }
+
   componentDidMount() {
     if (this.props.infinteScroll) {
       this.addScrollEventListener();
+      this.callSetPaginationIfNotOverflow();
+    }
+  }
+
+  componentDidUpdate() {
+    if (!this.overflowCalled && this.props.infinteScroll) {
+      this.callSetPaginationIfNotOverflow();
     }
   }
 
   componentWillUnmount() {
     const listTable = document.querySelector(`#${this.props.id} div.ant-table-body`);
     if (listTable) {
-      listTable.removeEventListener('scroll', this.onScrollListTable);
+      listTable.removeEventListener('scroll', this.onScrollThrottle);
     }
   }
 
   addScrollEventListener = () => {
     const listTable = document.querySelector(`#${this.props.id} div.ant-table-body`);
     if (listTable) {
-      listTable.addEventListener('scroll', this.onScrollListTable);
+      listTable.addEventListener('scroll', this.onScrollThrottle);
     }
   }
 
@@ -34,6 +48,21 @@ export default class CapTable extends React.Component { // eslint-disable-line r
     const currentScroll = event.target.scrollTop;
     if (currentScroll === maxScroll && !this.props.showLoader) {
       const offsetLimit = Object.assign(this.props.offset_limit);
+      offsetLimit.offset += offsetLimit.limit;
+      this.props.setPagination(offsetLimit);
+    }
+  }
+
+  callSetPaginationIfNotOverflow() {
+    const yscrollheight = this.props.scroll ? this.props.scroll.y : 0;
+    const listTable = document.querySelector(`#${this.props.id} div.ant-table-body`);
+    let isOverflow = false;
+    if (yscrollheight && yscrollheight < listTable.scrollHeight) {
+      isOverflow = true;
+    }
+    if (!isOverflow && this.props.setPagination && this.props.dataSource.length > 0) {
+      this.setPaginationCalled = true;
+      const offsetLimit = { ...this.props.offset_limit };
       offsetLimit.offset += offsetLimit.limit;
       this.props.setPagination(offsetLimit);
     }
