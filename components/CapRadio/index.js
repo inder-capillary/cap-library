7d7cd22cbf9c2b0a6fe718502e6aeebf98f81a2b
meharajcap <33620889+meharajcap@users.noreply.github.com>
2019-04-02 17:15:16 +0530

    Cap radio changes (#114)
    * added inductive text support for radio box
    
    * added inductive text support for radio box

--- components/CapRadio/index.js
+++ components/CapRadio/index.js
@@ -4,15 +4,29 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { Radio } from "antd";
+import CapHeading from '../CapHeading';
+
 import "./_capRadio.scss";
+
+const { CapHeadingSpan } = CapHeading;
+
 const classNames = require('classnames');
 export default class CapRadio extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const {className, children, ...rest} = this.props;
+    const {className, children, errorMessage, inductiveText, suffix, ...rest} = this.props;
     return (
-      <Radio {...rest} className={classNames("cap-radio-v2", className)}>
-        {children}
-      </Radio>
+      <div className={classNames("cap-radio-v2", className)}>
+        <Radio {...rest}>
+          <CapHeadingSpan className={classNames('title', { 'has-suffix': suffix })} type="h4">{children}</CapHeadingSpan>
+          {suffix}
+          {inductiveText && <CapHeading className="inductive-text" type="label3">{inductiveText}</CapHeading>}
+        </Radio>
+        {
+          className && className.indexOf('error') > -1 && errorMessage
+          && <div className="error-message">{errorMessage}</div>
+        }
+      </div>
+
     );
   }
 }
@@ -20,4 +34,7 @@ export default class CapRadio extends React.Component { // eslint-disable-line r
 CapRadio.propTypes = {
   children: PropTypes.any,
   className: PropTypes.string,
+  inductiveText: PropTypes.string,
+  errorMessage: PropTypes.string,
+  suffix: PropTypes.node,
 };
