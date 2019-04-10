67712e1b22a132043d78ad5ed29ae9e5dddfae6d
Sakshi-kataria-Capillary <42139409+Sakshi-kataria-Capillary@users.noreply.github.com>
2019-04-10 15:03:49 +0530

    resolve campaign and message bugs (#119)
    * resolve campaign and message bugs
    
    * code review

--- src/CapLinkDoc/index.js
+++ src/CapLinkDoc/index.js
@@ -8,16 +8,39 @@ import "./info.scss";
 
 const infoData = [
   {
+    key: 1,
     property: "href",
     description: "target of hyperlink",
     type: "string",
     default: "",
   },
   {
+    key: 2,
     property: "title",
     description: "content of  hyperlink",
     type: "string|ReactNode",
     default: "",
+  },
+  {
+    key: 3,
+    property: "onClick",
+    description: "content of  hyperlink",
+    type: "function",
+    default: "",
+  },
+  {
+    key: 4,
+    property: "prefix",
+    description: "Prefix content of link",
+    type: "string | Node",
+    default: "-",
+  },
+  {
+    key: 5,
+    property: "suffix",
+    description: "Suffix content of link",
+    type: "string | ReactNode",
+    default: "-",
   },
 ];
 
