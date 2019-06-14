b2b92dd793da1e6100be353a294d8ab0764770c8
Swathi8512 <swathi.venkatesh@capillarytech.com>
2019-06-14 13:53:08 +0530

    Minor bug fixes

--- components/CapHeader/index.js
+++ components/CapHeader/index.js
@@ -93,7 +93,7 @@ function CapHeader(props) {
       {
         props.prefix
       }
-      <div>
+      <div className="title-desc-container">
         {getTitleComponent()}
         {(description || withHiddenDescription) && titleDesc()}
       </div>
