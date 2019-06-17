0b01ea542dec068f95381d54f5aede6f603a2b54
Dhara Panchal <dhara.panchal@CLPT0953.local>
2019-06-17 08:17:24 +0530

    Added CapStatisticCard component

---
+++ components/CapStatisticCard/index.js
@@ -0,0 +1,34 @@
+/**
+*
+* CapStatisticCard
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CapRow from '../CapRow';
+import CapColumn from '../CapColumn';
+import CapHeading from '../CapHeading';
+import './_capStatisticCard.scss';
+
+function CapStatisticCard(props) {
+  const { statsItems } = props;
+  return (
+    <CapRow className="stats-card">
+      {statsItems.map((item, index) => (
+        <CapColumn span={4} className="stats-item" key={`stats-${item.text}-${index}`}>
+          <CapRow className="stats-text"><CapHeading type="h6">{item.text}</CapHeading></CapRow>
+          <CapRow className="stats-val"><CapHeading type="h3">{item.value}</CapHeading></CapRow>
+          {item.subText && <CapRow className="stats-sub-text"><CapHeading type="h6">{item.subText}</CapHeading></CapRow>}
+        </CapColumn>
+      ))}
+    </CapRow>
+  );
+}
+
+CapStatisticCard.propTypes = {
+  statsItems: PropTypes.array,
+};
+
+export default CapStatisticCard;
