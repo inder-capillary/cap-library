446e6d91b2e7bb56c05e97734635e1f3c151ab30
meharaj <meharaj.moulasab@capillerytech.com>
2019-07-01 16:23:11 +0530

    resolved conflicts

--- components/assets/svgIcons/Icons/Delete.js
+++ components/assets/svgIcons/Icons/Delete.js
@@ -8,10 +8,10 @@ const SvgDelete = (props) => (
     fill="currentColor"
     {...props}
   >
-      <path
-        id="delete_svg__a"
-        d="M16 9v10H8V9h8zm-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"
-      />
+    <path
+      id="delete_svg__a"
+      d="M16 9v10H8V9h8zm-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"
+    />
   </svg>
 );
 
