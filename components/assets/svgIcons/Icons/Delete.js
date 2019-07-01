73d1d301f1a1c7bb3bd3d08f7ca56fe9b219a7eb
nikhilpatelcaptech <46444330+nikhilpatelcaptech@users.noreply.github.com>
2019-07-01 12:18:49 +0530

    delete and drag svg file changes (#150)

--- components/assets/svgIcons/Icons/Delete.js
+++ components/assets/svgIcons/Icons/Delete.js
@@ -8,21 +8,10 @@ const SvgDelete = (props) => (
     fill="currentColor"
     {...props}
   >
-    <defs>
       <path
         id="delete_svg__a"
         d="M16 9v10H8V9h8zm-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"
       />
-    </defs>
-    <g fill="none" fillRule="evenodd">
-      <mask id="delete_svg__b" fill="#fff">
-        <use xlinkHref="#delete_svg__a" />
-      </mask>
-      <use fill="#000" fillRule="nonzero" xlinkHref="#delete_svg__a" />
-      <g fill="#091E42" mask="url(#delete_svg__b)">
-        <path d="M0 0h24v24H0z" />
-      </g>
-    </g>
   </svg>
 );
 
--- components/assets/svgIcons/Icons/Drag.js
+++ components/assets/svgIcons/Icons/Drag.js
@@ -8,21 +8,10 @@ const SvgDrag = (props) => (
     fill="currentColor"
     {...props}
   >
-    <defs>
       <path
         id="drag_svg__a"
         d="M20 10a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm-8 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm-8 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm16 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm-8 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm-8 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
       />
-    </defs>
-    <g fill="none" fillRule="evenodd">
-      <mask id="drag_svg__b" fill="#fff">
-        <use xlinkHref="#drag_svg__a" />
-      </mask>
-      <use fill="#FFF" xlinkHref="#drag_svg__a" />
-      <g fill="#7A869A" mask="url(#drag_svg__b)">
-        <path d="M0 0h24v24H0z" />
-      </g>
-    </g>
   </svg>
 );
 
