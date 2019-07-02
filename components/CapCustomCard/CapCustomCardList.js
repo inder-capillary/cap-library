ef2fe111fe978d827b6f303cd5c408a964678bee
meharajcap <33620889+meharajcap@users.noreply.github.com>
2019-07-02 11:13:10 +0530

    Custom card css changes (#152)
    * custom card css changes
    
    * resolved conflicts
    
    * code review changes

--- components/CapCustomCard/CapCustomCardList.js
+++ components/CapCustomCard/CapCustomCardList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import CapCustomCard from './CapCustomCard';
-import Row from '../CapRow';
-import Col from '../CapColumn';
+
 
 const clsPrefix = "cap-custom-card-list";
 
@@ -12,22 +11,22 @@ export default class CapCustomCardList extends React.Component {
     return (
       <div className={classNames(clsPrefix, type, className)}>
         {cardList && (
-          <Row gutter={16} className={classNames(`${clsPrefix}-row`)}>
+          <div className={classNames(`${clsPrefix}-row`)}>
             {
               cardList.map((data, i) => {
                 const { dataBelowCard, cardType, ...rest } = data;
                 const key = data.key || i;
                 return (
-                  <Col span={8} className={classNames(`${clsPrefix}-col`)}>
+                  <div className={classNames(`${clsPrefix}-col`)}>
                     <CapCustomCard key={key} type={cardType || type} {...rest} />
                     {
                       dataBelowCard && dataBelowCard
                     }
-                  </Col>
+                  </div>
                 );
               })
             }
-          </Row>
+          </div>
         )}
       </div>
     );
