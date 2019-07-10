18ed1ac3c0a5d7c94113b99fe051368687e0518e
Swathi8512 <swathi.venkatesh@capillarytech.com>
2019-07-10 13:41:31 +0530

    Support info popover for radio card

--- components/CapRadioCard/index.js
+++ components/CapRadioCard/index.js
@@ -9,44 +9,61 @@ import './_capRadioCard.scss';
 
 import { Card, Radio } from "antd";
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
+import classNames from 'classnames';
 import CapHeading from '../CapHeading';
 import CapIcon from '../CapIcon';
-const classNames = require('classnames');
+import CapTooltip from '../CapTooltip';
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 class CapRadioCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  getRadioButton = (pane) => {
+    const { cardHeight, selected, defaultValue } = this.props;
+    const { content, title, icon, value, ...restParams } = pane;
+    return (
+      <RadioButton style={{ height: cardHeight || '120px' }} key={value} value={value} {...restParams}>
+        <CapIcon type="check-filled" className="radio-card-checked" />
+        <Card>
+          {icon && (
+            <div className="radio-card-icon">
+              <div className={classNames('icon-container', { 'green-color': selected ? selected === value : defaultValue === value })}>
+                <div className="div-icon">
+                  {icon}
+                </div>
+              </div>
+            </div>
+          )}
+          <div className="radio-card-content-container">
+            <div className="radio-card-header"><CapHeading type="h4">{title}</CapHeading></div>
+            <div className="radio-card-content">
+              {' '}
+              {content}
+            </div>
+          </div>
+        </Card>
+      </RadioButton>
+    );
+  }
+
   render() {
     const { panes, className, selected, cardHeight, ...rest } = this.props;
     return (
       <RadioGroup {...rest} className={classNames("cap-radioCard-v2", className)}>
         {panes && (
-          panes.map((pane) => {
-            const { content, title, icon, value, ...restParams } = pane;
-            return (
-              <RadioButton style={{ height: cardHeight || '120px' }} key={value} value={value} {...restParams}>
-                <CapIcon type="check-filled" className="radio-card-checked" />
-                <Card>
-                  {icon && (
-                    <div className="radio-card-icon">
-                      <div className={classNames('icon-container', { 'green-color': selected ? selected === value : rest.defaultValue === value })}>
-                        <div className="div-icon">
-                          {icon}
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                  <div className="radio-card-content-container">
-                    <div className="radio-card-header"><CapHeading type="h4">{title}</CapHeading></div>
-                    <div className="radio-card-content">
-                      {' '}
-                      {content}
-                    </div>
-                  </div>
-                </Card>
-              </RadioButton>
-            );
-          }))}
+          panes.map((pane) => (
+              <>
+                {get(pane, 'toolTipProps.title')
+                  ? (
+                    <CapTooltip
+                      {...pane.tooltipProps}
+                    >
+                      {this.getRadioButton(pane)}
+                    </CapTooltip>
+                  ) : this.getRadioButton(pane)}
+              </>
+          ))
+        )}
       </RadioGroup>
 
     );
