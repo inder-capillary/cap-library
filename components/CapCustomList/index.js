d9773b85db36d909bc56d3b20a0d3262f62fdebe
sakshi-kataria-capillary <sakshi.kataria@capillarytech.com>
2019-12-08 16:57:15 +0530

    CapCustomList Dynamic For multiple content

--- components/CapCustomList/index.js
+++ components/CapCustomList/index.js
@@ -4,7 +4,7 @@
 *
 */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import isEmpty from 'lodash/isEmpty';
@@ -14,8 +14,10 @@ import CapIcon from '../CapIcon';
 import './_capCustomList.scss';
 import CapHeading from '../CapHeading';
 import {CAP_SPACE_16, CAP_SPACE_24, CAP_G07, CAP_SPACE_08} from "../styled/variables";
+import CapSpin from '../CapSpin';
 
 const classNames = require('classnames');
+
 const CapCustomSubHeader = styled(CapHeader)`
   padding-bottom:${CAP_SPACE_16};
 `;
@@ -29,39 +31,62 @@ const CapRectangle = styled.div`
   background-color: ${CAP_G07};
 `;
 
-
 const CapCustomButton = styled(CapButton)`
   padding-left: 0px;
   display: contents;
   text-align: left;
 `;
+
 const clsPrefix = 'cap-customList-v2';
+const {CapHeadingSpan} = CapHeading;
+
 function CapCustomList(props) {
-  const { list, className } = props;
+  const { list, className, loader, getList, contentId, type } = props;
   const [subList, loadSubList] = useState(
     {}
   );
-  const {subCategory, levelName, levelvalueCount} = subList;
+  const [totalCategoryCount, setTotalCategoryCount] = useState(0);
+  const {subCategory, levelName, levelValueCount} = subList;
+
+  const calculateTotalCategoryCount = (data) => {
+    setTotalCategoryCount(data.reduce((a, b) => b.subCategory.length + a, 0));
+  };
+
+  useEffect(() => {
+    getList(contentId, type);
+  }, [loader]);
+
+  useEffect(() => {
+    calculateTotalCategoryCount(list);
+  }, [list]);
   return (
-    <React.Fragment>
+    <CapSpin spinning={loader}>
       {isEmpty(subCategory)
         ? (
           <React.Fragment>
-            <CapCustomHeader
-              size="regular"
-              title={props.title}
-              className={classNames(clsPrefix, className)} />
-
+            <CapHeadingSpan
+              type="h3"
+              className={classNames(clsPrefix, className)}
+              style={{marginBottom: CAP_SPACE_24, display: 'inline-block'}}>
+              {props.title}
+            </CapHeadingSpan>
+            {totalCategoryCount > 0 && (
+              <CapHeadingSpan type="label5">
+          (
+                {totalCategoryCount}
+)
+              </CapHeadingSpan>
+            )}
             {list.map((pane) => (
               <CapCustomButton
                 className="category-select-btn"
                 type="flat"
-                onClick={() => loadSubList({levelName: pane.levelName, subCategory: pane.subCategory, levelvalueCount: pane.levelvalueCount})}
+                onClick={() => loadSubList({levelName: pane.levelName, subCategory: pane.subCategory, levelValueCount: pane.levelValueCount})}
               >
                 <CapCustomSubHeader
                   size="label1"
                   title={pane.levelName}
-                  description={pane.levelvalueCount}
+                  description={pane.levelValueCount}
                   suffix={(
                     <CapIcon
                       className="category-select-btn-suffix"
@@ -87,7 +112,7 @@ function CapCustomList(props) {
                   style={{ marginLeft: '-6px', paddingRight: CAP_SPACE_08}}
                 />
               )}
-              description={levelvalueCount}
+              description={levelValueCount}
             />
             {subCategory.map((category) => (
               <React.Fragment>
@@ -107,7 +132,7 @@ function CapCustomList(props) {
           </React.Fragment>
         )
       }
-    </React.Fragment>
+    </CapSpin>
 
   );
 }
@@ -117,6 +142,10 @@ CapCustomList.propTypes = {
   title: PropTypes.string,
   list: PropTypes.array,
   className: PropTypes.string,
+  loader: PropTypes.bool,
+  contentId: PropTypes.string,
+  getList: PropTypes.func,
+  type: PropTypes.string,
 };
 
 export default CapCustomList;
