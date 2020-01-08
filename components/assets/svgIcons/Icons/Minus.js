a447322dd086c3161fd76251f8651050deb5a2bd
archita-sarda <40684313+archita-sarda@users.noreply.github.com>
2020-01-08 19:46:39 +0530

    removing extra elements in minus svg (#206)

--- components/assets/svgIcons/Icons/Minus.js
+++ components/assets/svgIcons/Icons/Minus.js
@@ -9,21 +9,10 @@ function SvgMinus(props) {
       fill="currentColor"
       {...props}
     >
-      <defs>
-        <path
-          id="minus_svg__a"
-          d="M0 1.005a1 1 0 001.001 1.001H21a1.001 1.001 0 100-2.002H1a1 1 0 00-1 1.001z"
-        />
-      </defs>
-      <g fill="none" fillRule="evenodd" transform="translate(5 15)">
-        <mask id="minus_svg__b" fill="#fff">
-          <use xlinkHref="#minus_svg__a" />
-        </mask>
-        <use fill="#000" fillRule="nonzero" xlinkHref="#minus_svg__a" />
-        <g fill="#2466EA" mask="url(#minus_svg__b)">
-          <path d="M-5-15h32v32H-5z" />
-        </g>
-      </g>
+      <path
+        id="minus_svg__a"
+        d="M0 1.005a1 1 0 001.001 1.001H21a1.001 1.001 0 100-2.002H1a1 1 0 00-1 1.001z"
+      />
     </svg>
   );
 }
