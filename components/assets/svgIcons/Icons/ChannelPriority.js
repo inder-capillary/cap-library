e3b8e2d31760b3c0d9ca31d27ac0bf8673017a6f
sakshi-kataria-capillary <sakshi.kataria@capillarytech.com>
2020-02-25 15:28:09 +0530

    ChannelPriority icon changes

---
+++ components/assets/svgIcons/Icons/ChannelPriority.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+function SvgChannelPriority(props) {
+  return (
+    <svg
+      viewBox="0 0 47.55 47.55"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      {...props}
+    >
+      <path
+        d="M14 33v-8a1 1 0 011-1h5v-7.94A1 1 0 0121 15h6a1 1 0 011 1.06V20h5a1 1 0 011 1.08V33zm6-7h-4v5h4zm6-9h-4v14h4zm6 5h-4v9h4z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default SvgChannelPriority;
