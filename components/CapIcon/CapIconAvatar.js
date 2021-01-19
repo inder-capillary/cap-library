7893ff68c0c0337ef122efbba1b05aac124fe776
vignespacap <vignesh.palanisamy@capillarytech.com>
2021-01-19 19:40:47 +0530

    CAP-63292props with default value added

--- components/CapIcon/CapIconAvatar.js
+++ components/CapIcon/CapIconAvatar.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import './_capIcon.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./_capIcon.scss";
 import { LogoBackground } from "../assets/icons";
 
-const clsPrefix = 'cap-icon-v2';
+const clsPrefix = "cap-icon-v2";
 
 function CapIconAvatar(props) {
-  const { className, text, width, height, backgroundProps, labelProps } = props;
+  const {
+    className,
+    text,
+    width,
+    height,
+    backgroundProps = {},
+    labelProps = {},
+  } = props;
   const customClassName = `${clsPrefix}-avatar`;
 
   return (
     <div className={classNames(customClassName, className)}>
       <LogoBackground width={width} height={height} {...backgroundProps} />
-      <span className="text-label" {...labelProps}>{text}</span>
+      <span className="text-label" {...labelProps}>
+        {text}
+      </span>
     </div>
   );
 }
