cf14fe815a9d7b6d023f62a8603c19fb65af515b
vignespacap <vignesh.palanisamy@capillarytech.com>
2021-03-02 20:46:47 +0530

    CAP-64258 Cap Select | Virtual commit

--- components/CapCustomSelect/index.js
+++ components/CapCustomSelect/index.js
@@ -1,34 +1,37 @@
 /* eslint-disable no-unused-expressions */
 /**
-*
-* CapCustomSelect
-*
-*/
+ *
+ * CapCustomSelect
+ *
+ */
 
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { Popover } from "antd";
-import classNames from 'classnames';
-import findIndex from 'lodash/findIndex';
-import styled from 'styled-components';
+import classNames from "classnames";
+import findIndex from "lodash/findIndex";
+import styled from "styled-components";
+import { List } from "react-virtualized";
 import * as styledVars from "../styled/variables";
-import CapHeading from '../CapHeading';
-import CapIcon from '../CapIcon';
-import CapInput from '../CapInput';
-import LocaleHoc from '../LocaleHoc';
+import CapHeading from "../CapHeading";
+import CapIcon from "../CapIcon";
+import CapInput from "../CapInput";
+import LocaleHoc from "../LocaleHoc";
+import CapTag from "../CapTag";
 
-import './_capCustomSelect.scss';
+import "./_capCustomSelect.scss";
 
-const clsPrefix = 'cap-custom-select';
+const clsPrefix = "cap-custom-select";
+const TAG = "tag";
 
 const StyledCapHeading = styled(CapHeading)`
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
-
-class CapCustomSelect extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class CapCustomSelect extends React.Component {
+  // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
     this.state = {
@@ -39,32 +42,45 @@ class CapCustomSelect extends React.Component { // eslint-disable-line react/pre
 
   handleSearch = (e) => {
     this.setState({ searchText: e.target.value });
-  }
+  };
 
   onVisibleChange = (visible) => {
     this.setState({ visible });
     if (!visible) {
       this.setState({ visible: false, searchText: "" });
     }
-  }
+  };
 
   handleChange = (item) => {
-    const { onChange } = this.props;
-    this.setState({ visible: false });
-    onChange(item.value, item);
-  }
+    const { onChange, type, closeOnSelect = true, value } = this.props;
+    closeOnSelect && this.setState({ visible: false });
+    const selectValue = type === TAG ? [...value, item.value] : item.value;
+    onChange(selectValue, item);
+  };
+
+  onRemoveTag = (event, removedValue) => {
+    const { onChange, value } = this.props;
+    onChange([...value.filter((selectValue) => selectValue !== removedValue)]);
+    event.stopPropagation();
+  };
 
   getItems = () => {
-    const { options, value } = this.props;
+    const { options, value, type } = this.props;
     const { searchText } = this.state;
     return options.reduce((acc, item) => {
-      if (searchText === "" || item.label.toLowerCase().indexOf(searchText.toLowerCase()) !== -1) {
+      if (
+        (searchText === ""
+        || item.label.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)
+        && !(type === TAG && value.includes(item.value))
+      ) {
         acc.push(
           <StyledCapHeading
             type="h6"
             key={item.value}
             onClick={() => this.handleChange(item)}
-            className={classNames(`${clsPrefix}-select-item`, { selected: value === item.value })}
+            className={classNames(`${clsPrefix}-select-item`, {
+              selected: value === item.value,
+            })}
           >
             {item.label}
           </StyledCapHeading>
@@ -72,14 +88,46 @@ class CapCustomSelect extends React.Component { // eslint-disable-line react/pre
       }
       return acc;
     }, []);
-  }
+  };
+
+  getTagItems = () => {
+    const { options, value } = this.props;
+    const optionsKeyMap = options.reduce((acc, item) => {
+      acc[item.value] = item;
+      return acc;
+    }, {});
+    return value.map((selectValue) => optionsKeyMap[selectValue]);
+  };
 
   clearSearch = () => {
     this.setState({ searchText: "" });
-  }
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  rowRenderer = ({ index, isScrolling, key, style }, itemsList) => (
+    <div key={key} style={style}>
+      <>{itemsList[index]}</>
+    </div>
+  );
 
   render() {
-    const { options, value, showSearch, searchPlaceholder, selectPlaceholder, width, className, popoverClassName, disabled } = this.props;
+    const {
+      options,
+      value,
+      showSearch,
+      searchPlaceholder,
+      selectPlaceholder,
+      width,
+      className,
+      popoverClassName,
+      disabled,
+      type,
+      virtual = false,
+      virtualScrollHeight = 200,
+      virtualScrollWidth = 250,
+      rowHeight = 40,
+    } = this.props;
+
     const { visible, searchText } = this.state;
     const itemsHtml = this.getItems();
     const selectedItemIndex = findIndex(options, (item) => item.value === value);
@@ -112,22 +160,75 @@ class CapCustomSelect extends React.Component { // eslint-disable-line react/pre
                 onClear={this.clearSearch}
               />
             )}
-            {itemsHtml.length > 0 ? <div className={classNames(`${clsPrefix}-items-wrapper`)}>{itemsHtml}</div>
-              : (
-                <div className={classNames(`${clsPrefix}-no-results`)}>
-                  <CapIcon style={{ color: styledVars.CAP_G06 }} type="alert" />
-                  <div className={classNames(`${clsPrefix}-no-results-text`)}>No results found</div>
+            {itemsHtml.length > 0 ? (
+              <div className={classNames(`${clsPrefix}-items-wrapper`)}>
+                {virtual ? (
+                  <List
+                    rowCount={itemsHtml.length}
+                    width={virtualScrollWidth}
+                    height={virtualScrollHeight}
+                    rowHeight={rowHeight}
+                    rowRenderer={(optValue) => this.rowRenderer(optValue, itemsHtml)}
+                    overscanRowCount={5}
+                  />
+                ) : (
+                  itemsHtml
+                )}
+              </div>
+            ) : (
+              <div className={classNames(`${clsPrefix}-no-results`)}>
+                <CapIcon style={{ color: styledVars.CAP_G06 }} type="alert" />
+                <div className={classNames(`${clsPrefix}-no-results-text`)}>
+                  No results found
                 </div>
-              )
-            }
+              </div>
+            )}
           </Fragment>
         )}
       >
-        <div ref={(node) => { this.node = node; }} style={{ width: width || "100%" }} className={classNames(`${clsPrefix}-selection`, { open: !!visible }, className, { 'selection-disabled': disabled })}>
-          <div className={(`${clsPrefix}-selected-value`)}>
-            <StyledCapHeading type={value ? 'h5' : 'h6'} className={classNames({ 'selected-value-label-disabled': disabled })} title={selectedItemLabel}>{selectedItemLabel || selectPlaceholder}</StyledCapHeading>
+        <div
+          ref={(node) => {
+            this.node = node;
+          }}
+          style={{ width: width || "100%" }}
+          className={classNames(
+            `${clsPrefix}-selection`,
+            { open: !!visible },
+            { "type-tag": type === TAG },
+            className,
+            { "selection-disabled": disabled }
+          )}
+        >
+          <div className={`${clsPrefix}-selected-value`}>
+            {type !== TAG ? (
+              <StyledCapHeading
+                type={value ? "h5" : "h6"}
+                className={classNames({
+                  "selected-value-label-disabled": disabled,
+                })}
+                title={selectedItemLabel}
+              >
+                {selectedItemLabel || selectPlaceholder}
+              </StyledCapHeading>
+            ) : (
+              this.getTagItems().map(({ label, value: selectValue }) => (
+                <CapTag
+                  onClose={(event) => this.onRemoveTag(event, selectValue)}
+                  className={`${clsPrefix}-selected-tag`}
+                  key={selectValue}
+                  closable
+                >
+                  <StyledCapHeading type="label2" title={label}>
+                    {label}
+                  </StyledCapHeading>
+                </CapTag>
+              ))
+            )}
           </div>
-          <CapIcon type="chevron-down" className={classNames(`${clsPrefix}-arrow`)} />
+          <CapIcon
+            type="chevron-down"
+            className={classNames(`${clsPrefix}-arrow`)}
+          />
         </div>
       </Popover>
     );
@@ -138,6 +239,7 @@ CapCustomSelect.propTypes = {
   value: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
+    PropTypes.array,
   ]),
   options: PropTypes.array,
   onChange: PropTypes.func,
@@ -148,6 +250,15 @@ CapCustomSelect.propTypes = {
   className: PropTypes.string,
   popoverClassName: PropTypes.string,
   disabled: PropTypes.bool,
+  type: PropTypes.string,
+  virtual: PropTypes.bool,
+  closeOnSelect: PropTypes.bool,
+  virtualScrollHeight: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  virtualScrollWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  rowHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
-export default LocaleHoc(CapCustomSelect, { key: 'CapCustomSelect' });
+export default LocaleHoc(CapCustomSelect, { key: "CapCustomSelect" });
