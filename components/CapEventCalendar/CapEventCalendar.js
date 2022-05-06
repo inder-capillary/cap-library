237b25d9b577fea43410e5274ca3fde945ec3b9b
vignespacap <vignesh.palanisamy@capillarytech.com>
2022-05-06 13:44:35 +0530

    tooltip

--- components/CapEventCalendar/CapEventCalendar.js
+++ components/CapEventCalendar/CapEventCalendar.js
@@ -1,45 +1,75 @@
 import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
+import ReactDom from "react-dom";
 import moment from "moment";
+
+import CapTooltip from "../CapTooltip";
+
 import {
   getQuarterForDate,
   getMonthsForQuarter,
   getDaysOfMonth
 } from "./utils";
+import {
+  drawHeadingText,
+  drawDashedLines,
+  drawTodayLine,
+  drawLineSeperator
+} from "./drawUtils";
 
 const datas = [
-  {
-    event_id: 1,
-    title: "Event 1",
-    start: "2022/4/1",
-    end: "2022/6/30",
-    backgroundColor: "#e9e9ea"
-  },
-  {
-    event_id: 2,
-    title: "Event 2",
-    start: "2022/5/4",
-    end: "2022/5/29",
-    backgroundColor: "#c7e7c7"
-  },
-  {
-    event_id: 3,
-    title: "Event 3",
-    start: "2022/4/15",
-    end: "2022/5/1",
-    backgroundColor: "#feedc0"
-  }
+  [
+    {
+      event_id: 1,
+      title: "Event 1",
+      start: "2022/4/1",
+      end: "2022/4/9",
+      backgroundColor: "#e9e9ea"
+    },
+    {
+      event_id: 1.1,
+      title: "Event 1.1",
+      start: "2022/4/30",
+      end: "2022/5/15",
+      backgroundColor: "#feedc0"
+    }
+  ],
+  [
+    {
+      event_id: 2,
+      title: "Event 2",
+      start: "2022/5/4",
+      end: "2022/5/29",
+      backgroundColor: "#c7e7c7"
+    }
+  ],
+  [
+    {
+      event_id: 3,
+      title: "Event 3",
+      start: "2022/4/15",
+      end: "2022/5/1",
+      backgroundColor: "#feedc0"
+    }
+  ]
 ];
+const dashLineOffsetY = 20;
+const dateKeyFormat = "DD/MM/YYYY";
 
 const CapEventCalendar = props => {
   const [displayMonths] = useState(
     getMonthsForQuarter(getQuarterForDate(moment().format()))
   );
+  const noOfDaysInQuarter = 90;
 
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const pixelRatio = window.devicePixelRatio;
+
   const ref = useRef(null);
   const canvas = useRef(null);
+  const contextRef = useRef(null);
+  const drawObject = useRef(null);
+  const isTooltipVisible = useRef(null);
 
   const showBorder = (month, day, monthIndex) => {
     if (moment(month.end).date() === moment(day).date()) return true;
@@ -55,93 +85,202 @@ const CapEventCalendar = props => {
     setHeight(ref.current.clientHeight > 400 ? ref.current.clientHeight : 400);
   }, []);
 
-  useLayoutEffect(() => {
-    const context = canvas.current.getContext("2d");
-
-    let start = 0;
-    const itemWidth = width / 90;
-
+  const drawLayout = ({ context, width, height }) => {
     const dayObject = {};
+
+    let [rectX, rectY, rectHeight, rectWidth] = [
+      0,
+      0,
+      height,
+      width / noOfDaysInQuarter
+    ];
 
     displayMonths.forEach((month, monthIndex) => {
       const allDays = getDaysOfMonth(month.start);
 
       allDays.forEach(day => {
+        const rectMidX = rectX + rectWidth / 2;
         context.beginPath();
-        context.rect(start, 0, itemWidth, height);
+        context.rect(rectX, rectY, rectWidth, rectHeight);
         context.closePath();
 
-        dayObject[moment(day).format()] = {
-          x: start,
-          y: 0,
-          width: itemWidth,
-          height: height
+        const dayKey = moment(day).format(dateKeyFormat);
+        const today = moment().format(dateKeyFormat);
+        const isToday = dayKey === today;
+
+        dayObject[dayKey] = {
+          x: rectX,
+          y: rectY,
+          midX: rectMidX,
+          width: rectWidth,
+          height: rectHeight
         };
 
-        if (moment(day).day() === 1) {
-          context.beginPath();
-          context.fillStyle = "#ffff";
-          context.fillRect(start, 10, 20, 20);
-          context.fillStyle = "#091e42";
-          context.font = "12px Roboto";
-          context.textAlign = "center";
-          context.fillText(moment(day).format("D"), start + 10, 10 + 5);
-          context.closePath();
+        if (moment(day).day() === 1 && !isToday) {
+          const headingMarginBottom = 8;
+          drawHeadingText({
+            context,
+            x: rectMidX,
+            y: rectY,
+            height: dashLineOffsetY - headingMarginBottom,
+            text: moment(day).format("D")
+          });
 
-          context.beginPath();
-          context.setLineDash([5, 3]);
-          context.strokeStyle = "#b3bac5";
-          context.moveTo(start + itemWidth / 2, +20);
-          context.lineTo(start + itemWidth / 2, height);
-          context.stroke();
-          context.setLineDash([]);
-          context.closePath();
+          drawDashedLines({
+            context,
+            x: rectMidX,
+            y: rectY + dashLineOffsetY,
+            x1: rectMidX,
+            y1: rectHeight
+          });
         }
 
         if (showBorder(month, day, monthIndex)) {
-          context.beginPath();
-          context.strokeStyle = "#7a869a";
-          context.moveTo(start + itemWidth / 2, 0);
-          context.lineTo(start + itemWidth / 2, height);
-          context.stroke();
-          context.closePath();
+          drawLineSeperator({
+            context,
+            x: rectMidX,
+            y: rectY,
+            x1: rectMidX,
+            y1: rectHeight
+          });
         }
 
-        start += itemWidth;
+        rectX += rectWidth;
       });
     });
+    drawObject.current = { ...drawObject.current, day: dayObject };
+    return dayObject;
+  };
 
-    let heightCalc = 20;
-    datas.forEach(({ title, start, end, backgroundColor }) => {
-      const startRect = dayObject[moment(start).format()];
-      const endRect = dayObject[moment(end).format()];
-      context.beginPath();
-      context.fillStyle = backgroundColor;
-      context.fillRect(startRect.x, heightCalc, startRect.x + endRect.x, 20);
-      context.fillStyle = "#091e42";
-      context.font = "12px Roboto";
-      context.textAlign = "start";
-      context.fillText(title, startRect.x + 8, heightCalc + 14);
-      context.closePath();
-      heightCalc += 20 + 10;
-      console.log({ startRect, endRect });
+  const toggleTooltip = visible => {
+    const knob = document.getElementById("event-calendar-tool-tip-knob");
+    ReactDom.unmountComponentAtNode(knob);
+
+    if (visible) {
+      ReactDom.render(
+        <CapTooltip title="Today" visible>
+          <div />
+        </CapTooltip>,
+        knob
+      );
+    }
+    isTooltipVisible.current = visible;
+  };
+
+  const updateKnobPosition = ({ mouseX, mouseY } = {}) => {
+    const knob = document.getElementById("event-calendar-tool-tip-knob");
+    const { style } = knob;
+    if (mouseX) {
+      style.display = "block";
+      style.left = `${mouseX}px`;
+      style.top = `${mouseY}px`;
+    } else {
+      style.display = "none";
+      style.left = "-1000px";
+      style.top = "-1000px";
+    }
+  };
+
+  const handleTodayLine = ({ mouseX, mouseY } = {}) => {
+    const {
+      current: { day }
+    } = drawObject;
+    const context = contextRef.current;
+
+    const today = moment().format(dateKeyFormat);
+    const currentDayObj = day[today];
+
+    if (currentDayObj) {
+      const { x, y, midX, height } = currentDayObj;
+      return drawTodayLine({
+        context,
+        x: midX,
+        y: y + dashLineOffsetY,
+        x1: midX,
+        y1: height,
+        mouseX,
+        mouseY
+      });
+    }
+  };
+
+  const drawEvent = () => {
+    let eventStartY = 40;
+    const eventHeight = 20;
+    const eventRowGap = 10;
+    const {
+      current: { day }
+    } = drawObject;
+    const context = contextRef.current;
+
+    datas.forEach(row => {
+      row.forEach(({ title, start, end, backgroundColor }) => {
+        console.log(
+          moment(start).format(dateKeyFormat),
+          moment(end).format(dateKeyFormat)
+        );
+        const startRect = day[moment(start).format(dateKeyFormat)];
+        const endRect = day[moment(end).format(dateKeyFormat)];
+        if (startRect && endRect) {
+          context.beginPath();
+          context.fillStyle = backgroundColor;
+          context.fillRect(
+            startRect.midX,
+            eventStartY,
+            endRect.midX - startRect.midX,
+            eventHeight
+          );
+          context.fillStyle = "#091e42";
+          context.font = "12px Roboto";
+          context.textAlign = "start";
+          context.fillText(title, startRect.midX + 8, eventStartY + 14);
+          context.closePath();
+        }
+      });
+      eventStartY += eventHeight + eventRowGap;
     });
+  };
 
-    console.log({ dayObject });
+  useLayoutEffect(() => {
+    const context = canvas.current.getContext("2d");
+    contextRef.current = context;
+
+    drawLayout({ context, width, height });
+    handleTodayLine();
+    drawEvent();
   }, [width, height]);
 
   const displayWidth = Math.floor(pixelRatio * width);
   const displayHeight = Math.floor(pixelRatio * height);
   const style = { width, height };
 
+  const onMouseMove = event => {
+    const mouseX = event.nativeEvent.offsetX;
+    const mouseY = event.nativeEvent.offsetY;
+
+    if (handleTodayLine({ mouseX, mouseY })) {
+      if (!isTooltipVisible.current) {
+        updateKnobPosition({ mouseX, mouseY });
+        toggleTooltip(event);
+      }
+    } else {
+      toggleTooltip();
+    }
+  };
+
   return (
-    <div style={{ width: "100%", height: "100%" }} ref={ref}>
+    <div
+      style={{ width: "100%", height: "100%", position: "relative" }}
+      ref={ref}
+    >
       <canvas
         ref={canvas}
+        style={style}
         width={displayWidth}
         height={displayHeight}
-        style={style}
+        onMouseMove={onMouseMove}
       />
+      <div style={{ position: "absolute" }} id="event-calendar-tool-tip-knob" />
     </div>
   );
 };
---
+++ components/CapEventCalendar/drawUtils.js
@@ -0,0 +1,68 @@
+export const drawHeadingText = ({ context, x, y, height, text }) => {
+  context.beginPath();
+  context.fillStyle = "#ffff";
+  context.font = "12px Roboto";
+  const textWidth = context.measureText(text).width;
+
+  context.fillRect(x - textWidth / 2, y, textWidth, height);
+  context.fillStyle = "#091e42";
+  context.textAlign = "center";
+
+  context.fillText(text, x, y + height);
+  context.closePath();
+};
+
+export const drawCircle = ({ context, x, y, radius, color }) => {
+  context.beginPath();
+  context.fillStyle = color;
+  context.strokeStyle = color;
+  context.arc(x, y, radius, 0, 2 * Math.PI);
+  context.fill();
+  context.closePath();
+};
+
+export const drawDashedLines = ({ context, x, y, x1, y1 }) => {
+  context.beginPath();
+  context.setLineDash([5, 3]);
+  context.strokeStyle = "#b3bac5";
+
+  context.moveTo(x, y);
+  context.lineTo(x1, y1);
+
+  context.stroke();
+  context.setLineDash([]);
+  context.closePath();
+};
+
+export const drawTodayLine = ({ context, x, y, x1, y1, mouseX, mouseY }) => {
+  context.beginPath();
+  const radius = 4;
+  drawCircle({
+    context,
+    x,
+    y: y - radius,
+    radius,
+    color: "#2466ea"
+  });
+  context.strokeStyle = "#2466ea";
+  context.rect;
+  context.moveTo(x, y);
+  context.lineTo(x1, y1);
+  if (mouseX) {
+    context.rect(x - 3, y, 6, y1);
+    return context.isPointInPath(mouseX, mouseY);
+  }
+  context.stroke();
+  context.closePath();
+};
+
+export const drawLineSeperator = ({ context, x, y, x1, y1 }) => {
+  context.beginPath();
+  context.strokeStyle = "#7a869a";
+
+  context.moveTo(x, y);
+  context.lineTo(x1, y1);
+
+  context.stroke();
+  context.closePath();
+};
