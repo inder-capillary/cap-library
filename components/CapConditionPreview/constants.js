24c56b1db810c30cf2fb2f7f71725004467e42a6
varun-m-0310 <75354452+varun-m-0310@users.noreply.github.com>
2022-06-30 10:17:10 +0530

    Updated babel version - To resolve 'Cannot read property range of null' in template literals due to babel library (#492)
    * Updated babel version
    
    * Updated babel-jest to lates
    
    * Added cross-env
    
    Co-authored-by: Varun M <varunm@CLPT-0958.local>

--- components/CapConditionPreview/constants.js
+++ components/CapConditionPreview/constants.js
@@ -1,5 +1,6 @@
 export const LIST = 'LIST';
 export const MULTI_SELECT = 'MULTI_SELECT';
 export const NUMBER = 'NUMBER';
+export const PRODUCT_ATTRIBUTE = 'PRODUCT_ATTRIBUTE';
 
 export const SKU = 'SKU';
