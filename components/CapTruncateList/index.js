5d6938376227b79f76baf98e24306c79e5014aec
udayaditya-vr <72492221+udayaditya-vr@users.noreply.github.com>
2022-07-04 13:17:56 +0530

    Users/udayaditya vr/truncate list edit (#504)
    * 2.13.3
    
    * 2.13.8
    
    * 2.13.9
    
    * width changes to cap-truncate-list
    
    * 2.14.6
    
    * 2.14.20
    
    * width changes to cap-truncate-list
    
    Co-authored-by: 19uday <aditya.uday999@gmail.com>

--- components/CapTruncateList/index.js
+++ components/CapTruncateList/index.js
@@ -1,25 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import CapLabel from "../CapLabel";
 import CapTooltip from "../CapTooltip";
 import CapRow from "../CapRow";
 import LocaleHoc from "../LocaleHoc";
 
 const CapTruncateList = (props) => {
-  const { list, showNumber = 1, capLabelType, more: moreMsg, showTooltip } = props;
+  const { list, showNumber = 1, capLabelType, more: moreMsg, showTooltip, truncateToWidth } = props;
 
   const visibleList = list.slice(0, showNumber);
 
   return (
-    <CapRow type="flex" align="middle">
+    <CapRow
+      type="flex"
+      align="middle"
+      className={classNames({
+        'cap-truncate-list': true,
+      })}
+    >
       {visibleList.map((item, index) => (
-        <CapRow key={item}>
+        <CapRow
+          key={item}
+          className={classNames({
+            'truncate-text': truncateToWidth,
+            'first-item-container': true,
+          })}
+        >
           {index ? ", " : null}
           <CapLabel
             type={capLabelType}
             style={{
               marginRight: "4px",
             }}
+            className={classNames({
+              'truncate-text': truncateToWidth,
+            })}
           >
             {item}
           </CapLabel>
@@ -51,6 +67,7 @@ CapTruncateList.defaultProps = {
   showNumber: 1,
   capLabelType: "label1",
   showTooltip: true,
+  truncateToWidth: false,
 };
 
 CapTruncateList.propTypes = {
@@ -59,6 +76,7 @@ CapTruncateList.propTypes = {
   capLabelType: PropTypes.string,
   more: PropTypes.string,
   showTooltip: PropTypes.bool,
+  truncateToWidth: PropTypes.bool,
 };
 
 export default LocaleHoc(CapTruncateList, { key: "CapTruncateList" });
